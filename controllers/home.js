dc74f6660d82410e09bb36bec383151f3f76c5fe
IosuR <irecaldema14dw@ikzubirimanteo.com>
2016-02-03 09:39:18 +0000

    en desarrollo

--- controllers/home.js
+++ controllers/home.js
@@ -6,15 +6,19 @@
 exports.index = function(req, res) {
   //var io = req.app.io;
   var fecha = new Date();
-  console.log("render 3 "+fecha);
+  console.log("render "+fecha);
   
   res.render('index', {title: 'prueba extra'});
 };
 
 exports.url = function(req, res) {
+  // http://chat-socket-io-zubiri.c9users.io/chat/pruebas
   var chat = req.params.chat;
   var msg = req.params.mensaje;
   var io = req.app.io;
   console.log("get "+chat+" "+msg);
   var fecha = new Date();
+  io.sockets.on('connection', function(socket) {
+    socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', msg+" prueba "+fecha);
+  });  
 };
